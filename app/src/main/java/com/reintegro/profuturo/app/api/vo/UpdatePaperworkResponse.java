06a14b693e873946bbf5d0fccfbc42cc3b564a5e
joseuran <omar.sanchez@profuturo.com.mx>
2021-09-07 18:47:50 -0500

    Update UpdatePaperworkResponse.java

--- app/src/main/java/com/reintegro/profuturo/app/api/vo/UpdatePaperworkResponse.java
+++ app/src/main/java/com/reintegro/profuturo/app/api/vo/UpdatePaperworkResponse.java
@@ -3,11 +3,29 @@ package com.reintegro.profuturo.app.api.vo;
 import com.google.gson.annotations.SerializedName;
 
 public class UpdatePaperworkResponse {
+    public static class Response {
+        @SerializedName("success") private Boolean success;
+        @SerializedName("mensaje") private String message;
 
-    @SerializedName("success")
-    private Boolean success;
+        public Boolean getSuccess() {
+            return success;
+        }
 
-    public Boolean isSuccess() {
-        return success;
+        public String getMessage() {
+            return message;
+        }
+    }
+
+    @SerializedName("result")
+    private boolean result;
+    @SerializedName("actualizarTramiteResponse")
+    private UpdatePaperworkResponse.Response response;
+
+    public UpdatePaperworkResponse.Response getResponse() {
+        return response;
+    }
+
+    public boolean isResult() {
+        return result;
     }
 }
